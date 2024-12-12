6845bd2742e9ebb840b19fdbca6917de88e7ba14
Shen <863461783@qq.com>
2024-12-12 10:15:40 +0800

    update

--- app/api/users/typehero/[username]/route.ts
+++ app/api/users/typehero/[username]/route.ts
@@ -9,11 +9,11 @@ export async function GET(request: Request, { params }: { params: { username: st
   try {
     const response = await axios.get(`https://typehero.dev/@${params.username}`);
 
-    const reg = /<text x="135" y="110" text-anchor="middle" dominant-baseline="middle" class="fill-muted-foreground group-hover:underline">Total<\/text><text x="140" y="140" text-anchor="middle" dominant-baseline="middle" class="fill-foreground text-4xl font-bold">(\d+)<\/text><text x="140" y="170" text-anchor="middle" dominant-baseline="middle" class="fill-muted-foreground text-sm">of (\d+)<\/text>/;
+    const reg = /{\"totalCompleted\":(\d+),\"chartData\":\"\$([0-9,]+)\",\"totalChallenges\":(\d+),\"allCompletedHref\":\"\/@.*?\/completed\"}/;
     const match = reg.exec(response.data);
 
     if (match) {
-      output.message = `${match[1]}/${match[2]}`;
+      output.message = `${match[1]}/${match[3]}`;
     } else {
       output.message = 'Error';
     }

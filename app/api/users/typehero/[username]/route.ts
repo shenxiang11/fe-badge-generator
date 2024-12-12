5122abf116582e35737738edde18c6f8b2afe0ac
Shen <863461783@qq.com>
2024-12-12 10:35:30 +0800

    update

--- app/api/users/typehero/[username]/route.ts
+++ app/api/users/typehero/[username]/route.ts
@@ -1,15 +1,13 @@
 import axios from "axios";
 
 export async function GET(request: Request, { params }: { params: { username: string }}) {
-
   const output = {
     message: '',
   };
 
   try {
     const response = await axios.get(`https://typehero.dev/@${params.username}`);
-
-    const reg = /{\"totalCompleted\":(\d+),\"chartData\":\"\$([0-9,]+)\",\"totalChallenges\":(\d+),\"allCompletedHref\":\"\/@.*?\/completed\"}/;
+    const reg = /{\\"totalCompleted\\":(\d+),\\"chartData\\":\\"\$([0-9,]+)\\",\\"totalChallenges\\":(\d+),\\"allCompletedHref\\":\\"\/@.*?\/completed\\"}/;
     const match = reg.exec(response.data);
 
     if (match) {
